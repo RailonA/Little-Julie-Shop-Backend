a3dbe202f74f4a9d9a5819a1d4a0d8a70e228b70
RailonA <railonacosta@gmail.com>
2021-11-13 13:53:50 -0500

    CLean db to start adding Ancestry gem

--- app/models/category_group.rb
+++
@@ -1,3 +0,0 @@
-class CategoryGroup < ApplicationRecord
-    has_and_belongs_to_many :item
-end
--- db/migrate/20211113150702_create_category_groups.rb
+++
@@ -1,9 +0,0 @@
-class CreateCategoryGroups < ActiveRecord::Migration[6.1]
-  def change
-    create_table :category_groups do |t|
-      t.string :category
-
-      t.timestamps
-    end
-  end
-end
