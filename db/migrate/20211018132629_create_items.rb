76e5ff6290c9341514160b6cd2e675dd3bc7aaf7
RailonA <railonacosta@gmail.com>
2021-12-17 11:11:37 -0500

    Fix categoryreferences in item

--- db/migrate/20211018132629_create_items.rb
+++ db/migrate/20211018132629_create_items.rb
@@ -1,7 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-      t.references :categories, index: true, foreign_key: true
+      t.references :category, index: true, foreign_key: true
       t.string :name
       t.string :itemName
       t.string :itemPrice
