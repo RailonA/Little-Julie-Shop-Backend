b4ba9d357f99926274a011ffbb656c80d922128d
RailonA <railonacosta@gmail.com>
2021-12-27 12:29:42 -0500

    Try to fix models to be able to push to heroku

--- db/migrate/20211018132629_create_items.rb
+++ db/migrate/20211018132629_create_items.rb
@@ -2,7 +2,6 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
       t.references :category, index: true, foreign_key: true
-      # t.string :name
       t.string :itemName,  null: false
       t.string :itemPrice,  null: false
       t.string :itemDescription
