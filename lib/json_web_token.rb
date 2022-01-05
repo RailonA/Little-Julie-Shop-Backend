732e53b92273238c9b83e5450517dad298041d74
RailonA <railonacosta@gmail.com>
2022-01-05 12:52:15 -0500

    Fix json_web_token.rb

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,39 +1,39 @@
 # ***** LIVE SERVER *******
 
-# class JsonWebToken
-#   class << self
-#     # def encode(payload, exp = 24.hours.from_now)
-#     def encode(payload, exp = 10.days.from_now)
-#       payload[:exp] = exp.to_i
-#       # JWT.encode(payload, Rails.application.secrets.secret_key_base)
-#       JWT.encode(payload, ENV['TRIPS_SECRET'])
-#     end
-
-#     def decode(token)
-#       # body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
-#       body = JWT.decode(token, ENV['TRIPS_SECRET'])[0]
-#       HashWithIndifferentAccess.new body
-#
-#     rescue
-#       nil
-#     end
-#     #   end
-# end
-
-# ***** LOCAL SERVER *******
-
 class JsonWebToken
   class << self
-    def encode(payload, exp = 24.hours.from_now)
+    # def encode(payload, exp = 24.hours.from_now)
+    def encode(payload, exp = 10.days.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      # JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, ENV['TRIPS_SECRET'])
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      # body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      body = JWT.decode(token, ENV['TRIPS_SECRET'])[0]
       HashWithIndifferentAccess.new body
-    rescue StandardError
+
+    rescue
       nil
     end
-  end
+    #   end
 end
+
+# ***** LOCAL SERVER *******
+
+# class JsonWebToken
+#   class << self
+#     def encode(payload, exp = 24.hours.from_now)
+#       payload[:exp] = exp.to_i
+#       JWT.encode(payload, Rails.application.secrets.secret_key_base)
+#     end
+
+#     def decode(token)
+#       body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+#       HashWithIndifferentAccess.new body
+#     rescue StandardError
+#       nil
+#     end
+#   end
+# end
