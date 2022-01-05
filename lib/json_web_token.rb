43fbf9597050d0bcc36d1a7f9c987795d3981187
RailonA <railonacosta@gmail.com>
2022-01-05 13:30:22 -0500

    Fix error in json-web-token.rb

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -13,11 +13,12 @@ class JsonWebToken
       # body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
       body = JWT.decode(token, ENV['TRIPS_SECRET'])[0]
       HashWithIndifferentAccess.new body
-
+    # rubocop:disable Style/RescueStandardError
     rescue
       nil
     end
-    #   end
+    # rubocop:enable Style/RescueStandardError
+  end
 end
 
 # ***** LOCAL SERVER *******
@@ -32,7 +33,7 @@ end
 #     def decode(token)
 #       body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
 #       HashWithIndifferentAccess.new body
-#     rescue StandardError
+#     rescue
 #       nil
 #     end
 #   end
