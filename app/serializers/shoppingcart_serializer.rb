8960d386f52aff51d011c7a97e8d6e7524c3d6c7
RailonA <railonacosta@gmail.com>
2022-02-15 18:30:23 -0500

    Created one shoppingCart and displaying it in admin

--- app/serializers/shoppingcart_serializer.rb
+++ app/serializers/shoppingcart_serializer.rb
@@ -2,5 +2,5 @@ class ShoppingcartSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :item_id
 
   belongs_to :user, class_name: 'User'
-  belongs_to :item, class_name: 'Item'
+  has_many :item, class_name: 'Item'
 end
